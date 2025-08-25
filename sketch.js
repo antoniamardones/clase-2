51a27d92fc6c54092bf89ed54f6958048ed3228d
antoniamardones <antonia.mardones@ug.uchile.cl>
2025-08-25 18:04:01 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+ var valor = document.getElementById("pecinco").offsetWidth;
+
+            window.addEventListener("resize", function () {
+                valor = document.getElementById("pecinco").offsetWidth;
+            });
+
+            function setup() {
+                createCanvas(valor, valor, WEBGL).parent("#pecinco");
+                select("#nombre").html("Antonia Mardones");
+                select("#aprendizaje").html("la mayoría de los diferentes programas que existen y ojala sin estresarme tanto");
+            noStroke();
+            }
+            function draw() {
+               background(255, 216, 223);
+                orbitControl();
+                pointLight(255,255,255, valor/3, valor/2, 50);
+                specularMaterial(300);
+                shininess(50);
+                metalness(5);
+                fill(255, 82, 176);
+                cylinder (valor / 30, 50);
+            }
+
+            function windowResized() {
+                resizeCanvas(valor, valor);
+            }
